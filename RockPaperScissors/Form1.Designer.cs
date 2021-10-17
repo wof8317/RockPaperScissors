786f5a7c9813eafb47b39955598b1e920f6a0999
Felipe Woods <wof8317@gmail.com>
2021-10-17 14:35:39 -0500

    Add project files.

---
+++ RockPaperScissors/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace RockPaperScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissor = new System.Windows.Forms.Button();
+            this.btnUserChoice = new System.Windows.Forms.Button();
+            this.btnComputerChoice = new System.Windows.Forms.Button();
+            this.txtBoxOutcome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(94, 365);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(144, 59);
+            this.btnReset.TabIndex = 0;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(536, 365);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(144, 59);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnRock
+            // 
+            this.btnRock.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRock.Location = new System.Drawing.Point(39, 40);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(199, 176);
+            this.btnRock.TabIndex = 2;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaper.Location = new System.Drawing.Point(285, 40);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(199, 176);
+            this.btnPaper.TabIndex = 3;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissor
+            // 
+            this.btnScissor.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScissor.Location = new System.Drawing.Point(536, 40);
+            this.btnScissor.Name = "btnScissor";
+            this.btnScissor.Size = new System.Drawing.Size(199, 176);
+            this.btnScissor.TabIndex = 4;
+            this.btnScissor.Text = "Scissor";
+            this.btnScissor.UseVisualStyleBackColor = true;
+            this.btnScissor.Click += new System.EventHandler(this.btnScissor_Click);
+            // 
+            // btnUserChoice
+            // 
+            this.btnUserChoice.Enabled = false;
+            this.btnUserChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUserChoice.Location = new System.Drawing.Point(125, 248);
+            this.btnUserChoice.Name = "btnUserChoice";
+            this.btnUserChoice.Size = new System.Drawing.Size(113, 67);
+            this.btnUserChoice.TabIndex = 5;
+            this.btnUserChoice.UseVisualStyleBackColor = true;
+            // 
+            // btnComputerChoice
+            // 
+            this.btnComputerChoice.Enabled = false;
+            this.btnComputerChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComputerChoice.Location = new System.Drawing.Point(536, 248);
+            this.btnComputerChoice.Name = "btnComputerChoice";
+            this.btnComputerChoice.Size = new System.Drawing.Size(113, 67);
+            this.btnComputerChoice.TabIndex = 6;
+            this.btnComputerChoice.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxOutcome
+            // 
+            this.txtBoxOutcome.Enabled = false;
+            this.txtBoxOutcome.Location = new System.Drawing.Point(336, 272);
+            this.txtBoxOutcome.Name = "txtBoxOutcome";
+            this.txtBoxOutcome.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxOutcome.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBoxOutcome);
+            this.Controls.Add(this.btnComputerChoice);
+            this.Controls.Add(this.btnUserChoice);
+            this.Controls.Add(this.btnScissor);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RockPaperScissors";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissor;
+        private System.Windows.Forms.Button btnUserChoice;
+        private System.Windows.Forms.Button btnComputerChoice;
+        private System.Windows.Forms.TextBox txtBoxOutcome;
+    }
+}
+
---
+++ RockPaperScissors/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Form1 : Form
+    {
+        string Computer;//What the computer selects
+        string[] computer = { "Rock", "Paper", "Scissor" };//A list of strings to pick randomly
+        Random random = new Random();
+        int RandomType;
+        string PlayerPicks;//What the user picks
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            PlayerPicks = "Rock";//User picks rock
+            RandomType = random.Next(0, 3);//Random selection for computer
+            Computer = computer[RandomType];//FINALLY, Computer selects Rock, paper, or scissor at random.
+            Game();//Where the real fun begins
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            PlayerPicks = "Paper";//Same as Rock
+            RandomType = random.Next(0, 3);//Same as Rock
+            Computer = computer[RandomType];//Same as Rock
+            Game();//Where the real fun begins
+        }
+
+        private void btnScissor_Click(object sender, EventArgs e)
+        {
+            PlayerPicks = "Scissor";// Same as Rock and Paper
+            RandomType = random.Next(0, 3);
+            Computer = computer[RandomType];
+            Game();
+        }
+
+        void Game()
+        {
+            /*
+             * This function calculates the outcome of the game and who wins, loses, or if there is a tie, occurs.
+             * Example: if user chooses Rock, and the Computer randomly chooses Paper, both choices will be printed and
+             * print the outcome of "You Lose".
+             */
+            if (PlayerPicks == "Rock" && Computer == "Paper")
+            {
+                btnUserChoice.Text = PlayerPicks;
+                btnComputerChoice.Text = Computer;
+                txtBoxOutcome.Text = "You Lose";
+            }
+            else if (PlayerPicks == "Rock" && Computer == "Scissor")
+            {
+                btnUserChoice.Text = PlayerPicks;
+                btnComputerChoice.Text = Computer;
+                txtBoxOutcome.Text = "You Win";
+            }
+            else if (PlayerPicks == "Paper" && Computer == "Scissor")
+            {
+                btnUserChoice.Text = PlayerPicks;
+                btnComputerChoice.Text = Computer;
+                txtBoxOutcome.Text = "You Lose";
+            }
+            else if (PlayerPicks == "Paper" && Computer == "Rock")
+            {
+                btnUserChoice.Text = PlayerPicks;
+                btnComputerChoice.Text = Computer;
+                txtBoxOutcome.Text = "You Win";
+            }
+            else if (PlayerPicks == "Scissor" && Computer == "Rock")
+            {
+                btnUserChoice.Text = PlayerPicks;
+                btnComputerChoice.Text = Computer;
+                txtBoxOutcome.Text = "You Lose";
+            }
+            else if (PlayerPicks == "Scissor" && Computer == "Paper")
+            {
+                btnUserChoice.Text = PlayerPicks;
+                btnComputerChoice.Text = Computer;
+                txtBoxOutcome.Text = "You Win";
+            }
+            else
+            {
+                if (PlayerPicks == "Rock" && Computer == "Rock")
+                {
+                    btnUserChoice.Text = PlayerPicks;
+                    btnComputerChoice.Text = Computer;
+                    txtBoxOutcome.Text = "Tie";
+                }
+                else if (PlayerPicks == "Paper" && Computer == "Paper")
+                {
+                    btnUserChoice.Text = PlayerPicks;
+                    btnComputerChoice.Text = Computer;
+                    txtBoxOutcome.Text = "Tie";
+                }
+                else if (PlayerPicks == "Scissor" && Computer == "Scissor")
+                {
+                    btnUserChoice.Text = PlayerPicks;
+                    btnComputerChoice.Text = Computer;
+                    txtBoxOutcome.Text = "Tie";
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            btnComputerChoice.Text = "";
+            btnUserChoice.Text = "";
+            txtBoxOutcome.Clear();
+        }
+    }
+}
